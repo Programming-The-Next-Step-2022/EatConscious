030bafb35225e190682a75c035bca6ecd4df656d
anniejohansson <annie.johansson@student.uva.nl>
2022-06-02 10:50:35 +0200

    added wf dataset and created documentation

---
+++ EatConscious/R/data.R
@@ -0,0 +1,15 @@
+# Documentaiton for wf_data
+
+#' Water Footprint Dataset from the Water Footprint Network.
+#'
+#' A dataset containing the blue, green, and grey water footprint of various food items.
+#'
+#' @format A data frame where each food item has a column for:
+#' \describe{
+#'   \item{The global average water footprint}{}
+#'   \item{The average water footprint for each country}{}
+#'   \item{The average water footprint for each province / state within each country}{}
+#'
+#' }
+#' @source \url{https://waterfootprint.org/en/resources/waterstat/product-water-footprint-statistics/}
+"wf_data"
