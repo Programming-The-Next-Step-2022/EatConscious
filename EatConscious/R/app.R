0dfb87719f4f957fc0fb1c244bdac6142a7bbc02
anniejohansson <annie.johansson@student.uva.nl>
2022-06-03 12:25:32 +0200

    updated app

--- EatConscious/R/app.R
+++ EatConscious/R/app.R
@@ -1,4 +1,9 @@
 #' @import shiny
+#' @import shinythemes
+NULL
+
+#' @importFrom magick image_read
+NULL
 
 # GUI wrapped in function ----
 
@@ -11,39 +16,57 @@ GUI <- function() {
       navbarPage(title = "Eat Conscious",
                  # Welcome page -----
                  tabPanel(icon("home"),
-                          fluidRow(column(width = 8,
-                            tags$h3("Welcome to the Graphical User Interface of the package EatConscious!"),
-                            tags$body("EatConscious is an r package that retrieves and visualizes nutritional
+
+                          # General information -----
+                          fluidRow(
+                            column(width = 10,
+                                   tags$h1("Welcome to the Graphical User Interface of the package EatConscious!"),
+                                   tags$body("EatConscious is an r package that retrieves and visualizes nutritional
                                       and climate-impact data about most types of fresh produce."),
-                            tags$body("Use the navigation pane above to view different food-related information.")),
-                            column(width = 4)),
-                          fluidRow(column(width = 12,
-                            tags$h3(icon("water"), "What is a water footprint?"),
-                            tags$body("Over 2.7 billion people are affected by scarce water resources yearly.
+                                   HTML("<br>"),
+                                   tags$body("Use the navigation pane above to view different food-related information."))),
+
+                          # Water footprint information -----
+                          fluidRow(
+                            column(width = 8,
+                                   tags$h3(icon("tint", lib = "glyphicon"), "What is a water footprint?"),
+                                   tags$body("Over 2.7 billion people are affected by scarce water resources yearly.
                               The water footprint metric, created by Arjen Hoekstra, is a type of environmental
                               footprint that helps us to understand how the human consumption of different products
                               is affecting the earth's natural water sources. Making conscious choices about which products
                               to consume will aid in reducing our water foot print and ultimately preserve water for people and nature.
                               The water footprint data used in this package comes from The Water Footprint Network (https://waterfootprint.org/en/).
                               The dataset comprises the green, blue, and grey water footprints for a wide range of food
-                                              products on a global, national, and regional scale."))),
+                                              products on a global, national, and regional scale."),
+                                   HTML("<br><br><br>"),
+                                   gt_output("wf_types")),
+                            column(width = 4,
+                                   plotOutput("waterimg2"))),
                           tags$body("Read  more about the Water Footprint:"),
                           tags$a("https://waterfootprint.org/en/water-footprint/what-is-water-footprint/"),
+                          HTML("<br><br>"),
 
-                          wellPanel(tags$h3(icon("carrot"), "Nutritional Data"),
-                                    tags$body("You can also view nutritional data. This includes the nutritional measures
-                                              Calories (kcal / serving), Percent nutrients (the percentage of necessary daily
-                                              nutrients that one serving provides), Fat (grams / serving), Carbohydrates (grams / serving),
-                                              and Fibre (grams / serving)."))),
-
+                          # Nutritional data information -----
+                          fluidRow(
+                            column(width = 8,
+                                   tags$h3(icon("carrot"), "Nutritional Data"),
+                                   tags$body("You can also view and compare nutritional data of all sorts of food items."),
+                                   tags$body("This includes the nutritional measures"),
+                                   HTML("<ul>
+                                   <li>Calories (kcal / serving)</li>
+                                   <li>Percent nutrients (the percentage of necessary daily nutrients that one serving provides)</li>
+                                   <li>Fat (grams / serving)</li>
+                                   <li>Carbohydrates (grams / serving)</li>
+                                   <li>Fibre (grams / serving)</li>
+                                   </ul>")))),
 
                 # Global Water Footprint page -----
                 tabPanel("Global Water Footprint",
                          h3("Global Water Footprint Data"),
                          sidebarLayout(
                            sidebarPanel(textInput(inputId = "food",
-                                                  label = "search food",
-                                                  placeholder = "search food item here"),
+                                                  label = "Search food",
+                                                  placeholder = "Search food item here"),
                                         plotOutput(outputId = "pic")),
                            mainPanel(
                              tableOutput(outputId = "globaltable"),
@@ -55,7 +78,7 @@ GUI <- function() {
                          sidebarLayout(
                            sidebarPanel(textInput(inputId = "foodnational",
                                                   label = "Search food",
-                                                  placeholder = "search food item here"),
+                                                  placeholder = "Search food item here"),
                                         selectInput(inputId = "continent",
                                                     label = "Continent:",
                                                     choices = c("Americas", "Africa", "Asia", "Oceania", "Europe")),
@@ -80,10 +103,10 @@ GUI <- function() {
                                                   label = "Country:",
                                                   placeholder = "input country"),
                                         tags$body("Input the full country name starting with a Capital letter."),
-                                        tags$body(" "),
+                                        HTML("<br><br><br>"),
                                         textInput(inputId = "foodregional",
-                                                  label = "search food",
-                                                  placeholder = "search food item here")),
+                                                  label = "Search food",
+                                                  placeholder = "Search food item here")),
                            mainPanel(tabsetPanel(
                              tabPanel("Table", tableOutput(outputId = "regionaltable")),
                              tabPanel("Plot", plotOutput(outputId = "regionalplot",
@@ -97,8 +120,8 @@ GUI <- function() {
                            tabPanel("Search foods",
                                     sidebarLayout(
                                       sidebarPanel(textInput(inputId = "foodnutrition",
-                                                             label = "search food",
-                                                             placeholder = "search food item here")),
+                                                             label = "Search food",
+                                                             placeholder = "Search food item here")),
                                       mainPanel(tabsetPanel(
                                         tabPanel(title = "Table",
                                                  tableOutput("tablenutrition")),
@@ -108,10 +131,10 @@ GUI <- function() {
                                     sidebarLayout(
                                       sidebarPanel(textInput(inputId = "foodnutrition1",
                                                              label = "Food Item 1",
-                                                             placeholder = "search food item here"),
+                                                             placeholder = "Search food item here"),
                                                    textInput(inputId = "foodnutrition2",
                                                              label = "Food Item 2",
-                                                             placeholder = "search food item here"),
+                                                             placeholder = "Search food item here"),
                                                    selectInput(inputId = "nutritionmeasures_comp",
                                                                label = "Select nutrition measure",
                                                                choices = c("calories",
@@ -132,6 +155,29 @@ GUI <- function() {
     # Define server logic ----
     server <- function(input, output) {
 
+      output$waterimg2 <- renderPlot({
+        img_url <- "https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        pic <- image_read(img_url)
+        par(mar = c(0, 0, 0, 0))
+        plot(pic)
+      })
+      output$wf_types <- render_gt({
+        dat <- data.frame(Type = c("Green", "Blue", "Grey"),
+                          Explanation = c("Water acquired by the crop/plant directly from natural sources, such as precipitation.",
+                                          "Water that has been sourced from surface or groundwater resources.",
+                                          "The amount of freshwater that needs to be discharged into the water source to remove pollutants and meet water quality standards."))
+        gt(dat) %>%
+          tab_options(column_labels.hidden = TRUE)%>%
+          data_color(columns = Type,
+                     colors = c("lightblue", "darkseagreen", "slategrey"),
+                     apply_to = c("fill", "text"),
+                     autocolor_text = FALSE) %>%
+          data_color(columns = Explanation,
+                     colors = c("slategrey", "darkseagreen", "lightblue"),
+                     apply_to = c("fill", "text"),
+                     autocolor_text = FALSE,
+                     alpha = 0.3)
+      })
       output$pic <- renderPlot({
         food_pic(input$food)
       })
@@ -178,10 +224,13 @@ GUI <- function() {
 
 }
 
+GUI()
 
-
-
-
+library(shiny)
+library(shinythemes)
+library(gt)
+library(EatConscious)
+library(magick)
 
 
 
